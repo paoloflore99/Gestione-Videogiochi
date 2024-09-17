de143545fb9b1be6bcb9c452e98d378dbc5056f8
paolo flore <paoloflore937@gmail.com>
2024-09-17 19:53:40 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestione_Videogiochi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
